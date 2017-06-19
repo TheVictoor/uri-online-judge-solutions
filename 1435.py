94e0a13e644f7cf72a10435da894693493157c88
thevictoor <victoorsp@gmail.com>
2017-06-19 01:30:02 -0300

    Solução problema 1435

---
+++ 1435.py
@@ -0,0 +1,43 @@
+while 1:
+	size = int(input())
+	if size == 0:
+		break
+	mat = []
+	linha = []
+
+	#cria uma matriz SIZExSIZE e configura seus valores para 1
+	for i in range(size):
+		linha = []
+		for c in range(size):
+			linha += [1]
+		mat.append(linha)
+
+	if size / 2 > size // 2:
+		ctr = size // 2 + 1
+	else:
+		ctr = size // 2	
+	
+	for cn in range(1, ctr+1):
+		#para testes
+		#print('camada %d' %cn) 
+		for lin in range(cn - 1, size - (cn - 1)):
+			#para testes
+			#print('\tlinha %d' %lin)
+			for col in range(cn - 1, size - (cn - 1)):
+				#para teste
+				#print('\t\tcoluna %d'%col)
+				mat[lin][col] = cn
+	
+	#mostra matriz
+	for i in range(size):
+		for c in range(size):
+			if c == 0 and size == 1:
+				print('%3d' %mat[i][c])
+			elif c == 0:
+				print('%3d' %mat[i][c], end='')
+			elif c == size - 1:
+				print(' %3d' %mat[i][c])
+			else:
+				print(' %3d' %mat[i][c], end='')
+	print()
+

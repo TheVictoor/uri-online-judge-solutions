0856c506b585c6a795b120cc1c2108cf2e8c7357
thevictoor <victoorsp@gmail.com>
2017-06-27 02:55:39 -0300

    novas soluções em python

---
+++ 1933.py
@@ -0,0 +1,12 @@
+q = input()
+q = q.split(' ')
+
+a = int(q[0])
+b = int(q[1])
+
+if a == b: 
+	print(a)
+elif a > b:
+	print(a)
+elif a < b:
+	print(b)
---
+++ 1959.py
@@ -0,0 +1,9 @@
+q = input()
+q = q.split(' ')
+
+a = int(q[0])
+b = int(q[1])
+
+c = a * b
+
+print(c)

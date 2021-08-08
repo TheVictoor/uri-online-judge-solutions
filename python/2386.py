fff309b5194f251cbdcc6df9ff9cac95d9b9c2d8
Joao Victor (JV) <victoorsp@gmail.com>
2021-08-08 18:50:46 -0300

    feat: add 2386 solution

---
+++ python/2386.py
@@ -0,0 +1,13 @@
+THRESHOLD = 40_000_000
+
+telescope_area_in_mm = int(input())
+
+seeable_stars = 0
+
+n_stars = int(input())
+
+for i in range(n_stars):
+  if int(input()) * telescope_area_in_mm >= THRESHOLD:
+    seeable_stars += 1
+
+print(seeable_stars)

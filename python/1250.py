70f67f9b10990ef9a63603c3fe1004f3e155d2fd
Joao Victor (JV) <victoorsp@gmail.com>
2021-08-08 22:30:14 -0300

    feat: add new solutions

---
+++ python/1250.py
@@ -0,0 +1,19 @@
+n_tests = int(input())
+
+for i in range(n_tests):
+  n_shots = int(input())
+  shots = input().split()
+  actions = input()
+
+  shots_hit = 0
+
+  for i, shot in enumerate(shots):
+    shot_height = int(shot)
+
+    if shot_height > 2 and actions[i] == 'J':
+      shots_hit += 1
+    
+    if shot_height <= 2 and actions[i] == 'S':
+      shots_hit += 1
+
+  print(shots_hit)
---
+++ python/1573.py
@@ -0,0 +1,7 @@
+import math
+
+while True:
+  a, b, c = input().split()
+  if a == b == c == '0':
+    break
+  print(math.floor((int(a)*int(b)*int(c))**(1/3)))

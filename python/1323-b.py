5f547d0910e1a3765d0198f4ddf4761629f2b7c8
Joao Victor (JV) <victoorsp@gmail.com>
2021-08-21 12:11:39 -0300

    feat: new solutions

---
+++ python/1323-b.py
@@ -0,0 +1,11 @@
+
+while True:
+  x = int(input())
+  if x == 0:
+    break
+
+  n = 0
+  for i in range(x):
+    n += (x - i)**2
+
+  print(n)
---
+++ python/3049.py
@@ -0,0 +1,16 @@
+b = int(input())
+t = int(input())
+
+a = 70
+base = 160
+top = 160
+
+side_a = b + t
+side_b = (base - b) + (top - t)
+
+if side_a > side_b:
+  print(1)
+elif side_a < side_b:
+  print(2)
+else:
+  print(0)
